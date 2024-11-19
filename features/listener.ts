1dd6bcb4032c9b18f63ca82544ac1b5760367585
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-19 09:12:16 -0500

    feat: make the bot invite me instead of joining

--- features/listener.ts
+++ features/listener.ts
@@ -1,10 +1,11 @@
-import { slackApp, userSlackClient } from "../index";
+import { slackApp, slackClient } from "../index";
 
 async function listener() {
 	slackApp.event("channel_created", async ({ context, payload }) => {
 		console.log(`A new channel ${payload.channel.name} was created`);
-		await userSlackClient.conversations.join({
+		await slackClient.conversations.invite({
 			channel: payload.channel.id,
+			users: process.env.ADMINS?.split(",") || [],
 		});
 	});
 }
