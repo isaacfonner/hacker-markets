7ffa65d2c2164b47236f27e99bb5d0452f092173
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-19 11:09:05 -0500

    feat: automatically join new channels when created

--- features/listener.ts
+++ features/listener.ts
@@ -3,6 +3,11 @@ import { slackApp, slackClient } from "../index";
 async function listener() {
 	slackApp.event("channel_created", async ({ context, payload }) => {
 		console.log(`A new channel ${payload.channel.name} was created`);
+
+		await slackClient.conversations.join({
+			channel: payload.channel.id,
+		});
+
 		await slackClient.conversations.invite({
 			channel: payload.channel.id,
 			users: process.env.ADMINS?.split(",") || [],
